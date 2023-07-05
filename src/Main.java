74c10555c3e1a274efbbc9ef029da7b1de13d982
Davydov.P <davydov.works@gmail.com>
2023-07-04 21:36:43 -0400

    Добавил в код строчку вывода нового баланса

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int bill = 100;
-        int put = 2300;
+        int put = 5500;
         boolean putmoney = put >= 1000;
 
         int bonus;
@@ -12,8 +12,12 @@ public class Main {
             bonus = (0);
         }
         int your_bonus = put / 100;
+        int new_balance = bonus+bill+put;
 
-        System.out.println("Ваш бонус: " + bonus + " руб");
+        System.out.println();
+        System.out.println("Начисленно бонусных рублей: " + bonus + " руб");
+        System.out.println("Ваш баланс равен: " + new_balance + " руб");
+
 
     }
 }
